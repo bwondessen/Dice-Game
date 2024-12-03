42779ac35709cf8ece697d6fc5c6e36b0547011e
bwondessen <bwondessen23@gmail.com>
2024-12-02 19:06:05 -0600

    Create updateRadioOption function

--- script.js
+++ script.js
@@ -32,7 +32,13 @@ const rollDice = () => {
 const updateStats = () => {
     rollsElement.textContent = rolls;
     roundElement.textContent = round;
-}
+};
+
+const updateRadioOption = (index, score) => {
+    scoreInputs[index].disabled = false;
+    scoreInputs[index].value = score;
+    scoreSpans[index].textContent = `, score = ${score}`;
+};
 
 rollDiceBtn.addEventListener("click", () => {
     if (rolls === 3) {
