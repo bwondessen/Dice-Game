4e63c0669368fea0bbd7d26f4917820846dbbb9f
bwondessen <bwondessen23@gmail.com>
2024-12-02 18:47:42 -0600

    Generate and display random numbers when Roll the dice button is clicked

--- script.js
+++ script.js
@@ -16,14 +16,31 @@ let score = 0;
 let round = 1;
 let rolls = 0;
 
+const rollDice = () => {
+    diceValuesArr = [];
+
+    for (let i = 0; i < 5; i++) {
+        const randomDice = Math.floor(Math.random() * 6) + 1;
+        diceValuesArr.push(randomDice);
+    };
+
+    listOfAllDice.forEach((dice, index) => {
+        dice.textContent = diceValuesArr[index];
+    });
+};
+
+rollDiceBtn.addEventListener("click", () => {
+    rollDice();
+});
+
 rulesBtn.addEventListener("click", () => {
     isModalShowing = !isModalShowing;
 
     if (isModalShowing) {
+        rulesBtn.textContent = "Hide rules";
         rulesContainer.style.display = "block";
-        rulesBtn.innerText = "Hide rules";
     } else {
+        rulesBtn.textContent = "Show rules";
         rulesContainer.style.display = "none";
-        rulesBtn.innerText = "Show rules";
     }
-})
+});
